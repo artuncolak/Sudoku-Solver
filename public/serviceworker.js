de31913adc1aa59ceec28ed5e72b016928fe001c
Artun Çolak <artuncolak97@gmail.com>
2020-09-09 16:09:45 +0300

    Added PWA

---
+++ public/serviceworker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html", "*.js", "*.css"];
+
+const self = this;
+
+//  Install SW
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+//  Listener for requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("index.html"));
+    })
+  );
+});
+
+//  Activate the SW
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        // eslint-disable-next-line consistent-return
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
